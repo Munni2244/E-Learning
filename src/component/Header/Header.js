576d835d4bdc5bb122ae92186bfcc2a6c63acd41
Munni2244 <munnikhatun51544@gmail.com>
2021-10-05 21:27:03 +0600

    add title

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -5,14 +5,16 @@ const Header = () => {
     return (
         <div className="banner ">
            <Container>
+              
            <Row>
                <Col lg={9} md={6} xs={12} className="banner-details">
+               <h1 className="my-5"> E Learning <i class="fas fa-graduation-cap text-warning"></i></h1>
                 <h3>Be more self aware, understand your impact on others. Know how to cope with the unexpected. Learn how to succeed even under pressure. </h3>
                 <button className="btn btn-secondary mt-3">See More Information</button>
                 </Col>
 
                 <Col lg={3} md={6} xs={12}>
-                <h1></h1>
+                
                 </Col>
             </Row>
            </Container>
